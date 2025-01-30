009f1cfaa3ee4c25359db12fd598e310b412ed8e
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-01-30 14:15:34 +0900

    ログインとログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
   end
@@ -16,5 +17,11 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '1111'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :email, :password, :password_confirmation, :last_name, :first_name,
+                                             :last_kana, :first_kana, :birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: '有効なメールアドレスを入力してください' }
+    validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, if: lambda {
+      password.present?
+    }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :first_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字カタカナを使用してください' }
+    validates :last_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字カタカナを使用してください' }
+    validates :birth_date
+  end
 end
