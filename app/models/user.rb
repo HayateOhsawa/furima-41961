5e4a21c81c015981e07976379cb20bdeb9852c31
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-01-31 15:02:34 +0900

    パスワードのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   with_options presence: true do
     validates :nickname
-    validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, if: lambda {
-      password.present?
-    }
+    validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, if: -> { password.present? }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :first_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字カタカナを使用してください' }
