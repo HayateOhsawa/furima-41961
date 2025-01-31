ef458cf6952f2ec87467412499d7ab1754b4683c
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-01-31 15:33:20 +0900

    長音記号の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
     validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, if: -> { password.present? }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :first_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角文字カタカナを使用してください' }
-    validates :last_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角文字カタカナを使用してください' }
+    validates :first_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字カタカナを使用してください' }
+    validates :last_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字カタカナを使用してください' }
     validates :birth_date
   end
 end
