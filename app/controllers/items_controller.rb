abc8106638796475e976ac6a879776f3e0e20927
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-04 09:33:09 +0900

    コントローラーの設定・修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:edit, :show]
+  before_action :check_user, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -41,6 +42,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def check_user
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
 end
