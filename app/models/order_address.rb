0fac2ec0c25b8eadcc6d405fe86efea53d58feea
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-05 12:46:00 +0900

    フォームやバリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,8 +2,13 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
-  validates :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :phone_number, presence: true
-  validates :prefecture_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id,
+              :city, :addresses
+    validates :phone_number, numericality: { only_integer: true }
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Please select a prefecture' }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
