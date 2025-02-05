6f79b7116821ab4c8d6dafb28550ddb50b15b398
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-05 11:15:08 +0900

    ビューの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+
+  validates :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :phone_number, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses,
+                   building: building, phone_number: phone_number)
+  end
 end
