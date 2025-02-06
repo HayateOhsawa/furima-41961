6a6dcebf7e7701568576d6ec06dbe6305a813d3a
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-06 10:45:53 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,6 +3,8 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :postal_code,     format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id,   numericality: { other_than: 1, message: 'Select' }
     validates :city
